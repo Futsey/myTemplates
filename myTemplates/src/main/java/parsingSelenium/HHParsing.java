3b5f640aa32f2e8ff28d4cbb8fb4882f64f80a8c
Futsey <vjqubn2021!>
2022-01-27 10:26:52 +0500

    Bot update + begining of new project(Parser on Selenium)

---
+++ myTemplates/src/main/java/parsingSelenium/HHParsing.java
@@ -0,0 +1,23 @@
+package parsingSelenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HHParsing {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\Program Files\\JetBrains\\selenium\\chromedriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://hh.ru/");
+        WebElement element = driver.findElement(By.xpath("//div[@data-page-analytics-experiment-event='vacancy_search_region']"));
+        element.click();
+        Thread.sleep(3000);
+        WebElement input = driver.findElement(By.xpath("//input[@data-qa='area-search-input']"));
+        String inputTown = "Тарко-Сале";
+        input.sendKeys(inputTown);
+        input.submit();
+    }
+}
--- myTemplates/src/main/java/yandexPraktikum/HelperBotMyVersion.java
+++ myTemplates/src/main/java/yandexPraktikum/HelperBotMyVersion.java
@@ -7,7 +7,7 @@ public class HelperBotMyVersion {
     public static void main(String[] args) {
         EventsForHelperBot bot = new EventsForHelperBot();
         System.out.println("Jarvis V0.1 greetengs you. Sup?");
-        EventsForHelperBot.events(bot, 11, 23);
+        EventsForHelperBot.events(bot, 13, 23);
     }
 }
 
