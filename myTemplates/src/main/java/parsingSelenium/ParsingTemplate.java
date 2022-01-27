3718d5f5dae17c2f1dfe4dd19697cf492a9002cb
Futsey <vjqubn2021!>
2022-01-27 10:31:57 +0500

    ParsingTemplate

--- myTemplates/src/main/java/parsingSelenium/ParsingTemplate.java
+++ myTemplates/src/main/java/parsingSelenium/ParsingTemplate.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class HHParsing {
+public class ParsingTemplate {
 
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver",
