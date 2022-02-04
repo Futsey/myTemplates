914333729da8f44943061018bb351b601325418b
Andrew Petrushin <shmopls@gmail.com>
2022-02-04 14:17:22 +0500

    ArrayListTemplates

--- myTemplates/src/test/java/collectionTemplates/arrayList/ListOperationTemplatesTest.java
+++ myTemplates/src/test/java/collectionTemplates/arrayList/ListOperationTemplatesTest.java
@@ -1,5 +1,6 @@
 package collectionTemplates.arrayList;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -12,6 +13,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 public class ListOperationTemplatesTest {
+
+    private List<String> list = new ArrayList<>();
+    private List<String> list1 = new ArrayList<>();
 
     @Test
     public void whenGetNull() {
@@ -92,5 +96,114 @@ public class ListOperationTemplatesTest {
         list.add("third");
         boolean result = ListOperationTemplates.uniqueElement(list, "fourth");
         assertThat(result, is(false));
+    }
+
+    @Before
+    public void setUpChangePosition() {
+        list.add("one");
+        list.add("two");
+        list.add("three");
+        list.add("four");
+        list.add("five");
+        list.add("six");
+        list.add("seven");
+        list.add("eight");
+        list.add("nine");
+    }
+
+    @Test
+    public void changePosition() {
+        int index = 3;
+        List<String> result = ListOperationTemplates.repositionElement(list, index);
+        assertThat(result.size(), is(8));
+        assertThat(result.get(index), is("nine"));
+    }
+
+    @Test
+    public void changePositionNone() {
+        int index = 20;
+        List<String> result = ListOperationTemplates.repositionElement(list, index);
+        assertThat(result.size(), is(8));
+        assertThat(result.contains("nine"), is(false));
+    }
+
+    @Test
+    public void containsElement() {
+        List<String> left = new ArrayList<>();
+        left.add("first");
+        left.add("second");
+        left.add("third");
+        List<String> right = new ArrayList<>();
+        right.add("fourth");
+        right.add("second");
+        right.add("fifth");
+        String check = "second";
+        int result = ListOperationTemplates.addAllElementsList(left, right, check);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void containsElementInFirstList() {
+        List<String> left = new ArrayList<>();
+        left.add("first");
+        left.add("second");
+        left.add("third");
+        List<String> right = new ArrayList<>();
+        right.add("fourth");
+        right.add("fifth");
+        String check = "second";
+        int result = ListOperationTemplates.addAllElementsList(left, right, check);
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void split() {
+        List<String> left = new ArrayList<>();
+        left.add("one");
+        left.add("two");
+        left.add("three");
+        left.add("four");
+        left.add("five");
+        left.add("six");
+        left.add("seven");
+        left.add("eight");
+        left.add("nine");
+        left.add("ten");
+        List<String> middle = List.of(
+                "one", "two", "four", "five", "seven", "eight", "nine"
+        );
+        List<String> right = List.of(
+                "two", "five", "nine"
+        );
+        List<String> rsl = ListOperationTemplates.splitterList(left, middle, right);
+        assertThat(rsl, is(List.of("one", "four", "seven", "eight")));
+    }
+
+    @Before
+    public void setUpGetElementsBetweenIndexes() {
+        list1.add("one");
+        list1.add("two");
+        list1.add("three");
+        list1.add("four");
+        list1.add("five");
+        list1.add("six");
+        list1.add("seven");
+        list1.add("eight");
+        list1.add("three");
+        list1.add("ten");
+    }
+
+    @Test
+    public void getElementsBetweenIndexes() {
+        List<String> result = ListOperationTemplates.getElementsBetweenIndexes(list1, "three");
+        List<String> expected = List.of("three", "four", "five", "six", "seven", "eight");
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void getElementsBetweenIndexesEmptyList() {
+        List<String> result = ListOperationTemplates.getElementsBetweenIndexes(list1, "five");
+        List<String> expected = List.of();
+        assertThat(result, is(expected));
     }
 }
